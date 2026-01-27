de22d738cbb414fc5ad051e915a476b7c51c74b0
Samarth Sharma <samarthsharma351@gmail.com>
2026-01-27 13:32:41 -0800

    chore: Improves variable names.

--- leetcode/prefix-sum/560-subarray-equals.k.cpp
+++ leetcode/prefix-sum/560-subarray-equals.k.cpp
@@ -3,20 +3,20 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int count = 0, n = nums.size(), sum = 0;
         // we store frequency here 
-        unordered_map<int, int> um;
+        unordered_map<int, int> frequency;
 
         // base case - to handle the case when sum itself is equal to k
-        um[0] = 1;
+        frequency[0] = 1;
         
         for(int i = 0; i < n; i++){
             sum += nums[i];
 
-            if(um.find(sum - k) != um.end()){
-                count += um[sum - k];
+            if(frequency.find(sum - k) != frequency.end()){
+                count += frequency[sum - k];
             }
 
             // increase the count of number of times a sum occurs in the array
-            um[sum]++;
+            frequency[sum]++;
         }
 
         return count;
