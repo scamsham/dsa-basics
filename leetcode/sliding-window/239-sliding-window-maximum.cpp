27ef65b0e07c1e0ad22a44ac6ef2c19ffc6631c3
Samarth Sharma <samarthsharma351@gmail.com>
2026-01-29 20:55:45 -0800

    chore: Solves problem 239.

---
+++ leetcode/sliding-window/239-sliding-window-maximum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> result;
+        int n = nums.size();
+
+
+        for(int i = 0; i < n; i++){
+            // 1) Remove indices that are out of this window [i-k+1, i]
+            while(!dq.empty() && nums[dq.back()] <= nums[i]){
+                dq.pop_back();
+            }
+
+            // 2) Maintain decreasing order in dq (front is max)
+            while(!dq.empty() && i - dq.front() >= k){
+                dq.pop_front();
+            }
+    
+            // 3) Add current index
+            dq.push_back(i);
+
+            // 4) Window formed -> record max
+            if (i >= k - 1) {
+                result.push_back(nums[dq.front()]);
+            }
+        }
+
+        return result;
+    }
+};
