721645748fd0249f84eb471a1a236781ba35138a
Samarth Sharma <samarthsharma351@gmail.com>
2026-02-04 22:24:58 -0800

    chore: Adds description.

--- leetcode/graphs/329-longest-increasing-path-in-matrix-hard.cpp
+++ leetcode/graphs/329-longest-increasing-path-in-matrix-hard.cpp
@@ -1,4 +1,9 @@
 /*
+    Level: HARD
+    Given an m x n integers matrix, return the length of the longest increasing path in matrix.
+    From each cell, you can either move in four directions: left, right, up, or down.
+    You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed).
+
     https://leetcode.com/problems/longest-increasing-path-in-a-matrix/submissions/1908775181/
 */
 
