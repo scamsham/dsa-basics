8ffe1afbfc9031bf818682550dcc4912d319e489
Samarth Sharma <samarthsharma351@gmail.com>
2026-02-12 18:08:38 -0800

    chore: Adds better logic to next greater element code.

--- leetcode/stack/496-next-greater.cpp
+++ leetcode/stack/496-next-greater.cpp
@@ -7,7 +7,7 @@ public:
 
         // if all elements are unique we use map else we use a vector array
         unordered_map<int, int> um;
-
+        
         for(int i = 0; i < n; i++){
             while(!s.empty() && nums2[i] > nums2[s.top()]){
                 int index = s.top();
@@ -18,14 +18,16 @@ public:
             s.push(i);
         } 
 
+        while(!s.empty()){
+            int index = s.top();
+            s.pop();
+            int x = nums2[index];
+            um[x] = -1;
+        }
+
         vector<int> v;
         for(int &x: nums1){
-            cout<<x<<" --> "<<um[x]<<endl;
-            if(um[x] == 0){
-                v.push_back(-1);
-            }else{
-                v.push_back(um[x]);
-            }
+            v.push_back(um[x]);    
         }
            
         return v;
