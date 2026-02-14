f9d428c38cd9d784883b7634a34c6ab2d30fd606
Samarth Sharma <samarthsharma351@gmail.com>
2026-02-14 00:17:38 -0800

    chore: Solves next permutation problem leetcode.

---
+++ leetcode/arrays/31-next-permutation.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int pivot = -1, n = nums.size();
+        if(n == 1){
+            return ;
+        }
+
+        for(int j = n - 2; j >= 0; j--){
+            if(nums[j] < nums[j + 1]){
+                pivot = j;
+                break;
+            }
+        }
+
+        if(pivot == -1){
+            // already sorted so we reverse the array
+            reverse(nums.begin(), nums.end());
+            return ;
+        } 
+
+        // find the right most element greater than pivot element
+        for(int j = n - 1; j > pivot; j--){
+            if(nums[j] > nums[pivot]){
+                swap(nums[j], nums[pivot]);
+                break;
+            }
+        }
+
+        reverse(nums.begin() + pivot + 1, nums.end());
+
+        return ;
+    }
+};
